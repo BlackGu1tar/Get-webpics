74bdf19645a401393a99e870c323ee14c0ef74d9
BlackGu1tar <kgog0011@gmail.com>
2019-09-18 10:39:28 +0800

    Update 91forum.py

--- 91forum.py
+++ 91forum.py
@@ -12,7 +12,7 @@ import sys
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:59.0) Gecko/20100101 Firefox/59.0',}
 
-url_domain = 'https://f.wonderfulday30.live/viewthread.php?tid='
+url_domain = 'https://f.wonderfulday29.live/viewthread.php?tid='
 
 
 
@@ -23,26 +23,28 @@ def getHtml(url):
     return img_keys
 
 
-
+#start 351146, 351200
 #拼接完整URL下载
-for i in range(350918, 350928): #256805
+for i in range(351000, 351005): #256805
     all_url = url_domain + str(i)
     url_head = 'http://pic.w26.rocks/attachments'
-    html = requests.get(all_url,headers =headers).content
-    keys = re.findall('<h1>(.*?)</h1>', html)
-    try:
-        fileName = keys[0].decode('utf-8').encode('gbk')
-    except Exception as e:
-        print e + 'Not Found!!!.......'
-    if not os.path.exists(fileName):
-        os.mkdir(fileName)
-        for u in getHtml(all_url):
-            img_url = url_head + u
-            response = requests.get(img_url,headers=headers)
-            with open(fileName + '/' + img_url.split('/')[-1],'wb') as f: #打印图片到本地
-                f.write(response.content)
-                f.flush()
-                print img_url +'     *Downling Success!*'
+    html_code = requests.get(all_url,headers =headers)
+    if not html_code.status_code == 404:
+        html = requests.get(all_url,headers =headers).content
+        keys = re.findall('<h1>(.*?)</h1>', html)
+        try:
+            fileName = keys[0].decode('utf-8').encode('gbk')
+        except Exception as e:
+            print '91Forum Page Access Denied!!--------' + 'ID: ' + str(i)
+        if not os.path.exists(fileName):
+            os.mkdir(fileName)
+            for u in getHtml(all_url):
+                img_url = url_head + u
+                response = requests.get(img_url,headers=headers)
+                with open(fileName + '/' + img_url.split('/')[-1],'wb') as f: #打印图片到本地
+                    f.write(response.content)
+                    f.flush()
+                    print img_url +'     *Downling Success!*'
 
 
 
