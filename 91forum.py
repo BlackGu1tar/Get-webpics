113cdb9713fb190b01e07f4e90b3ffe666736b30
BlackGu1tar <kgog0011@gmail.com>
2019-09-18 14:32:45 +0800

    修复特殊字符问题

--- 91forum.py
+++ 91forum.py
@@ -24,27 +24,35 @@ def getHtml(url):
 
 
 #start 351146, 351200
-#拼接完整URL下载
-for i in range(351000, 351005): #256805
+
+for i in range(203276, 300000): #256805
     all_url = url_domain + str(i)
     url_head = 'http://pic.w26.rocks/attachments'
     html_code = requests.get(all_url,headers =headers)
     if not html_code.status_code == 404:
+        print '[+] Current Task ID : ' + str(i)
         html = requests.get(all_url,headers =headers).content
         keys = re.findall('<h1>(.*?)</h1>', html)
         try:
             fileName = keys[0].decode('utf-8').encode('gbk')
-            fileName = re.sub('[\/:*?"<>|]','-',fileName)
+            fileName = re.sub('[\/:*?"<>|.~]','-',fileName)
         except Exception as e:
-            print '91Forum Page Access Denied!!--------' + 'ID: ' + str(i)
-        if not os.path.exists(fileName):
+            #print '[+] 91Forum Page Access Denied!!--------' + 'ID: ' + str(i)
+            pass
+        if not os.path.exists(fileName) and len(fileName) == 0:
+            pass
+        elif not os.path.exists(fileName) and len(fileName) > 0:
             os.mkdir(fileName)
+        if len(getHtml(all_url)) == 0:
+            print '[+] Current Page is not have Pics...'
+        elif len(getHtml(all_url)):
             for u in getHtml(all_url):
                 img_url = url_head + u
                 response = requests.get(img_url,headers=headers)
                 with open(fileName + '/' + img_url.split('/')[-1],'wb') as f: #打印图片到本地
                     f.write(response.content)
                     f.flush()
-                    print img_url +'     *Downling Success!*'
+                    print '[+] ' + img_url +'     *Downling Success!*'
+
 
 
